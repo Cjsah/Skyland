7be15b9597a7805619c3244bc0d6f3251f4913be
Cjsah <cjsah@cjsah.com>
2024-06-14 11:14:41 +0800

    update

--- common/src/main/java/net/cjsah/skyland/generator/SkylandChunkGenerator.java
+++ common/src/main/java/net/cjsah/skyland/generator/SkylandChunkGenerator.java
@@ -1,16 +1,12 @@
 package net.cjsah.skyland.generator;
 
-import com.google.common.collect.Sets;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.cjsah.skyland.mixin.NoiseChunkAccessor;
-import net.minecraft.SharedConstants;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.WorldGenRegion;
-import net.minecraft.util.Mth;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.NoiseColumn;
 import net.minecraft.world.level.StructureManager;
@@ -21,7 +17,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.Aquifer;
 import net.minecraft.world.level.levelgen.Beardifier;
 import net.minecraft.world.level.levelgen.BelowZeroRetrogen;
@@ -29,12 +24,10 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.NoiseChunk;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
-import net.minecraft.world.level.levelgen.NoiseSettings;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.blending.Blender;
 
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
@@ -86,7 +79,7 @@ public class SkylandChunkGenerator extends ChunkGenerator {
     }
 
     private void doCreateBiomes(Blender blender, RandomState random, StructureManager structureManager, ChunkAccess chunk) {
-        NoiseChunk noiseChunk = chunk.getOrCreateNoiseChunk((chunkAccess) -> this.createNoiseChunk(chunkAccess, structureManager, blender, random));
+        NoiseChunk noiseChunk = chunk.getOrCreateNoiseChunk((chunkAccess) -> this.  createNoiseChunk(chunkAccess, structureManager, blender, random));
         BiomeResolver biomeResolver = BelowZeroRetrogen.getBiomeResolver(blender.getBiomeResolver(this.biomeSource), chunk);
         chunk.fillBiomesFromNoise(biomeResolver, ((NoiseChunkAccessor) noiseChunk).invokeCachedClimateSampler(random.router(), this.settings.value().spawnTarget()));
     }
@@ -105,31 +98,7 @@ public class SkylandChunkGenerator extends ChunkGenerator {
 
     @Override
     public CompletableFuture<ChunkAccess> fillFromNoise(Executor executor, Blender blender, RandomState random, StructureManager structureManager, ChunkAccess chunk) {
-        NoiseSettings noiseSettings = this.settings.value().noiseSettings().clampToHeightAccessor(chunk.getHeightAccessorForGeneration());
-        int i = noiseSettings.minY();
-        int j = Mth.floorDiv(i, noiseSettings.getCellHeight());
-        int k = Mth.floorDiv(noiseSettings.height(), noiseSettings.getCellHeight());
-        if (k <= 0) {
-            return CompletableFuture.completedFuture(chunk);
-        } else {
-            int l = chunk.getSectionIndex(k * noiseSettings.getCellHeight() - 1 + i);
-            int m = chunk.getSectionIndex(i);
-            Set<LevelChunkSection> set = Sets.newHashSet();
-
-            for (int n = l; n >= m; --n) {
-                LevelChunkSection levelChunkSection = chunk.getSection(n);
-                levelChunkSection.acquire();
-                set.add(levelChunkSection);
-            }
-
-            return CompletableFuture.supplyAsync(Util.wrapThreadWithTaskName("wgen_fill_noise", () -> chunk), Util.backgroundExecutor()).whenCompleteAsync((chunkAccess, throwable) -> {
-
-                for (LevelChunkSection levelChunkSection : set) {
-                    levelChunkSection.release();
-                }
-
-            }, executor);
-        }
+        return CompletableFuture.completedFuture(chunk);
     }
 
     @Override
