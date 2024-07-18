418be3c7fb9c4113e02cf5992b55af6ebcd1ac77
Gugle <administration@ztxy666.cn>
2024-07-18 15:44:50 +0800

    添加 Anvilcraft 联动入口

---
+++ common/src/main/java/net/cjsah/skyland/integration/anvilcraft/AnvilcraftIntegration.java
@@ -0,0 +1,9 @@
+package net.cjsah.skyland.integration.anvilcraft;
+
+import dev.anvilcraft.lib.integration.Integration;
+
+public class AnvilcraftIntegration implements Integration {
+    @Override
+    public void apply() {
+    }
+}
