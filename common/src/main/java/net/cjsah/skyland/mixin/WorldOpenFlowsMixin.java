562cdd371d4d39ed92c058fe00b7e71cf645e808
Gugle <administration@ztxy666.cn>
2024-07-29 18:28:52 +0800

    移除禁用实验性设置警告，修改竹块配方

--- common/src/main/java/net/cjsah/skyland/mixin/WorldOpenFlowsMixin.java
+++ common/src/main/java/net/cjsah/skyland/mixin/WorldOpenFlowsMixin.java
@@ -2,28 +2,8 @@ package net.cjsah.skyland.mixin;
 
 import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(WorldOpenFlows.class)
 public class WorldOpenFlowsMixin {
-    @ModifyVariable(
-        method = "loadLevel",
-        at = @At("HEAD"),
-        argsOnly = true,
-        index = 4
-    )
-    private boolean removeAdviceOnLoad(boolean original) {
-        return false;
-    }
 
-    @ModifyVariable(
-        method = "confirmWorldCreation",
-        at = @At("HEAD"),
-        argsOnly = true,
-        index = 4
-    )
-    private static boolean removeAdviceOnCreation(boolean original) {
-        return true;
-    }
 }
