60b0251609b278172e76428eb5c7f068a7431781
弭忧萌虾 <2211479860@qq.com>
2024-11-07 19:47:15 +0800

    增加竹子破基岩，初始空间结构，更改刷怪半径为4

---
+++ common/src/main/java/net/cjsah/skyland/mixin/BambooSaplingBlockMixin.java
@@ -0,0 +1,17 @@
+package net.cjsah.skyland.mixin;
+
+import net.minecraft.world.level.block.BambooSaplingBlock;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BambooSaplingBlock.class)
+public class BambooSaplingBlockMixin {
+    @Redirect( method = "isValidBonemealTarget",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z",opcode = Opcodes.GETFIELD))
+    private boolean isAirOrBedrock( BlockState blockState ){
+        return blockState.equals( Blocks.AIR.defaultBlockState() ) || blockState.equals( Blocks.BEDROCK.defaultBlockState() );
+    }
+}
