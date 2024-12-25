f4ab1ef8f140c2243c35fa60d519251280c250b9
Cjsah <cjsah@cjsah.net>
2024-12-25 23:36:40 +0800

    update

--- common/src/main/java/net/cjsah/skyland/Skyland.java
+++ common/src/main/java/net/cjsah/skyland/Skyland.java
@@ -1,6 +1,5 @@
 package net.cjsah.skyland;
 
-import dev.anvilcraft.lib.event.EventManager;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -10,7 +9,6 @@ public class Skyland {
     public static final Logger LOGGER = LoggerFactory.getLogger(Skyland.class);
     public static final String MOD_ID = "anvilcraft_skyland";
     public static final ResourceLocation SKYLAND_ID = Skyland.of("skyland");
-    public static final EventManager EVENT_BUS = new EventManager();
 
     public static @NotNull ResourceLocation of(String name) {
         return new ResourceLocation(MOD_ID, name);
